4df9944b224dcf4cc17d4cad379a205039643563
ValeroTomas <128714032+ValeroTomas@users.noreply.github.com>
2023-04-05 11:50:32 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,19 +2,34 @@
 #include <stdlib.h>
 
 int azar();
-int cargarNum(int);
+int cargarNum();
+int sumaNum(int);
 void devolver(int, int, int);
+void multiNum(int);
 
 int main()
 {
     int n1, n2, n3;
+
+    //PUNTO1
     /*num1 = azar();
     printf("\nNumero random: %d", num1);*/
-    n1 = cargarNum(n1);
-    n2 = cargarNum(n2);
-    n3 = cargarNum(n3);
-    devolver(n1, n2, n3);
-    return 0;
+
+    //PUNTO2
+    /*n1 = cargarNum();
+    n2 = cargarNum();
+    n3 = cargarNum();
+    devolver(n1, n2, n3);*/
+
+    //PUNTO3
+    /*n1 = cargarNum();
+    sumaNum(n1);
+    return 0;*/
+
+    //PUNTO4
+    /*n1 = 5;
+    multiNum(n1);*/
+
 }
 
 
@@ -86,9 +101,31 @@ void devolver(int num1, int num2, int num3)
     }
 }
 
-int cargarNum(int num1)
+int cargarNum()
 {
+    int num1;
     printf("\nIngrese un numero entero:  ");
     scanf("%d", &num1);
     return num1;
+}
+
+int sumaNum(int num1){
+    int suma = 0;
+    int i;
+    for(i = 0; i < num1; i++){
+        printf("\n|| %d + %d  =  %d ||\n", suma, i, suma+i);
+        suma = suma + i;
+    }
+    printf("\n|| RESULTADO = %d ||\n", suma);
+    return suma;
+}
+
+void multiNum(int num1){
+    int mult = 0;
+    int i, res;
+
+    for(i = 1; i < 11; i++){
+        res = num1 * i;
+        printf("\n|| %d * %d  =  %d ||\n", num1, i, res);
+    }
 }
